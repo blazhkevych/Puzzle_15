13ea96a03c53fad0a1b0401b1b7ad6123f9323bd
blazhkevych.v <blazhkevych.v@gmail.com>
2024-03-21 10:52:24 +0200

    # The game is done

--- app/src/main/java/com/example/puzzle_15/MainActivity.java
+++ app/src/main/java/com/example/puzzle_15/MainActivity.java
@@ -9,57 +9,81 @@ import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.Toast;
 
-
 @SuppressLint("MissingInflatedId")
+/**
+ * Moves the tile with the given value to the empty space, if the tile is adjacent to the empty space.
+ * @param value The value of the tile to move.
+ * @return The new coordinates of the empty space.
+ */
 public class MainActivity extends AppCompatActivity {
-
+    // The game logic for the "15 Puzzle" game.
     Puzzle_15 game = new Puzzle_15();
 
+    /**
+     * This method is called when the activity is starting.
+     * It initializes the game and sets up the user interface.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after previously being shut down then this Bundle contains the data it most recently supplied in onSaveInstanceState(Bundle). Note: Otherwise it is null.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // The GridLayout for the game board.
         GridLayout grid = findViewById(R.id.gridLayout);
-        //if (grid == null) return;
 
-        //grid.removeAllViews();
+        // Initialize the game.
         game.init();
 
+        // Set up the game board.
         for (int i = 0; i < 4; ++i) {
             for (int j = 0; j < 4; ++j) {
 
+                // Get the value of the tile at the current position.
                 int value = game.getValue(j, i);
                 if (value == 0) continue;
 
+                // Create a new button for the tile.
                 Button btn = new Button(this);
                 btn.setText(Integer.toString(value));
                 btn.setHeight(300);
 
+                // Set the position of the button in the GridLayout.
                 GridLayout.Spec row = GridLayout.spec(i);
                 GridLayout.Spec col = GridLayout.spec(j);
 
+                // Set the layout parameters for the button.
                 GridLayout.LayoutParams lp = new GridLayout.LayoutParams(row, col);
                 lp.setMargins(10, 10, 10, 10);
                 grid.addView(btn, lp);
 
+                // Set the click listener for the button.
                 btn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
 
+                        // Get the clicked button.
                         Button btn = (Button) view;
+                        // Get the value of the clicked button.
                         int value = Integer.parseInt((String) btn.getText());
 
+                        // Move the clicked tile to the empty space.
                         Puzzle_15.Coord coord = game.go(value);
 
+                        // If the move was valid, update the position of the button.
                         if (coord.isValid()) {
 
+                            // Set the new position of the button in the GridLayout.
                             GridLayout.Spec row = GridLayout.spec(coord.x);
                             GridLayout.Spec col = GridLayout.spec(coord.y);
 
+                            // Set the new layout parameters for the button.
                             GridLayout.LayoutParams lp = new GridLayout.LayoutParams(row, col);
+                            lp.setMargins(10, 10, 10, 10);
                             btn.setLayoutParams(lp);
 
+                            // If the game is won, show a toast message.
                             if (game.isWin()) {
                                 Toast.makeText(MainActivity.this, "You win!", Toast.LENGTH_SHORT).show();
                             }
--- app/src/main/java/com/example/puzzle_15/Puzzle_15.java
+++ app/src/main/java/com/example/puzzle_15/Puzzle_15.java
@@ -4,8 +4,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * This class represents the game logic for the game "15 Puzzle".
+ * It contains methods for initializing the game, checking the win condition, and moving the tiles.
+ */
 public class Puzzle_15 {
 
+    // The game field represented as a 2D array.
     private int[][] gameField = {
             {1, 2, 3, 4},
             {5, 6, 7, 8},
@@ -13,31 +18,52 @@ public class Puzzle_15 {
             {13, 14, 15, 0}
     };
 
+    /**
+     * This class represents a coordinate on the game field.
+     */
     public static class Coord {
 
         public int x;
         public int y;
 
+        /**
+         * Default constructor. Initializes the coordinate to (-1, -1).
+         */
         public Coord() {
             this(-1, -1);
         }
 
+        /**
+         * Constructor with parameters. Initializes the coordinate to the given values.
+         *
+         * @param x The x-coordinate.
+         * @param y The y-coordinate.
+         */
         public Coord(int x, int y) {
             this.x = x;
             this.y = y;
         }
 
+        /**
+         * Checks if the coordinate is valid (i.e., within the game field).
+         *
+         * @return true if the coordinate is valid, false otherwise.
+         */
         public boolean isValid() {
-
             return x > -1 && x < 4 && y > -1 && y < 4;
         }
     }
 
+    /**
+     * Constructor for the Puzzle_15 class. Initializes the game field.
+     */
     public Puzzle_15() {
-
         init();
     }
 
+    /**
+     * Initializes the game field with a random arrangement of tiles.
+     */
     public void init() {
         List<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < 16; i++) {
@@ -52,6 +78,11 @@ public class Puzzle_15 {
         }
     }
 
+    /**
+     * Checks if the current arrangement of tiles is a winning arrangement.
+     *
+     * @return true if the arrangement is winning, false otherwise.
+     */
     public boolean isWin() {
         for (int i = 0; i < 16; i++) {
             if (gameField[i / 4][i % 4] != i + 1) {
@@ -61,17 +92,27 @@ public class Puzzle_15 {
         return true;
     }
 
+    /**
+     * Returns the value at the given coordinates on the game field.
+     *
+     * @param x The x-coordinate.
+     * @param y The y-coordinate.
+     * @return The value at the given coordinates.
+     */
     public int getValue(int x, int y) {
-
         return gameField[y][x];
     }
 
+    /**
+     * Finds the coordinates of the given value on the game field.
+     *
+     * @param value The value to find.
+     * @return The coordinates of the value.
+     */
     public Coord findValue(int value) {
-
         for (int i = 0; i < 4; ++i) {
             for (int j = 0; j < 4; ++j) {
                 if (gameField[j][i] == value) {
-
                     return new Coord(j, i);
                 }
             }
@@ -79,9 +120,13 @@ public class Puzzle_15 {
         return new Coord();
     }
 
-    // TODO: В методе go() вы меняете местами выбранное число и ноль, но не проверяете, можно ли это сделать.
-    // В игре "Пятнашки" можно менять местами только соседние числа.
-    public Coord go(int value) { //
+    /**
+     * Moves the tile with the given value to the empty space, if the tile is adjacent to the empty space.
+     *
+     * @param value The value of the tile to move.
+     * @return The new coordinates of the empty space.
+     */
+    public Coord go(int value) {
         Coord zeroCoord = findValue(0);
         Coord coord = findValue(value);
 
